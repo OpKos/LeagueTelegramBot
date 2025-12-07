9d574b93c7a1ed0dbb092bac12b6a753e0efce38
OpKos <koparik71@gmail.com>
2025-12-08 01:46:24 +1000

    Arranged functions in sensible order

--- app/telegram_main.py
+++ app/telegram_main.py
@@ -11,29 +11,37 @@ def main() -> None:
     application = Application.builder().token(token).write_timeout(30).read_timeout(30).connect_timeout(300).build()
 
     application.add_handler(CommandHandler("register", register_command))
-    application.add_handler(CommandHandler("start_table", start_table_command))
-    application.add_handler(CommandHandler("next_table", next_table_command))
-    application.add_handler(CommandHandler("update_game_status", update_game_status_command))
-    application.add_handler(CommandHandler("get_settings", get_settings_command))
-    application.add_handler(CommandHandler("get_logs", get_logs_command))
-    application.add_handler(CommandHandler("force_reveal", force_reveal_command))
+    application.add_handler(CommandHandler("set_language", set_language))
     application.add_handler(CommandHandler(["player_info", "my_games"], get_player_info_command))
-    application.add_handler(CommandHandler("table_info", get_table_info_command))
-    application.add_handler(CommandHandler("start_status_message", start_status_message_command))
+
+    application.add_handler(CommandHandler("set_chat", set_chat))
+
+    application.add_handler(CommandHandler("start_table", start_table_command))
+
+    application.add_handler(CommandHandler("next_table", next_table_command))
+    application.add_handler(CommandHandler("all_tables", all_tables_command))
+
     application.add_handler(CommandHandler("set_time", set_time_command))
     application.add_handler(CommandHandler("remove_time", remove_time_command))
     application.add_handler(CommandHandler("timetable", timetable_command))
-    application.add_handler(CommandHandler("status", status_command))
+
+    application.add_handler(CommandHandler("get_settings", get_settings_command))
+    application.add_handler(CommandHandler("get_logs", get_logs_command))
+
     application.add_handler(CommandHandler("lobby", lobby_command))
     application.add_handler(CommandHandler("pantheon", pantheon_command))
-    application.add_handler(CommandHandler("set_language", set_language))
+
+    application.add_handler(CommandHandler("update_game_status", update_game_status_command))
+    application.add_handler(CommandHandler("force_reveal", force_reveal_command))
+    application.add_handler(CommandHandler("table_info", get_table_info_command))
+
+    application.add_handler(CommandHandler("start_status_message", start_status_message_command))
+    application.add_handler(CommandHandler("status", status_command))
+
     application.add_handler(CommandHandler("update_event_players", update_event_players_command))
     application.add_handler(CommandHandler("create_seating", create_seating_command))
-    application.add_handler(CommandHandler("next_table", next_table_command))
-    application.add_handler(CommandHandler("all_tables", all_tables_command))
     application.add_handler(CommandHandler("reveal_new_tables", reveal_new_tables))
     application.add_handler(CommandHandler("seating_image", seating_image_command))
-    application.add_handler(CommandHandler("set_chat", set_chat))
 
     application.add_handler(CallbackQueryHandler(chat_button, pattern=r"^TC"))
     application.add_handler(CallbackQueryHandler(ready_button, pattern=r"^RB"))
